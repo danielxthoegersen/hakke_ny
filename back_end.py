e396b6b1b0e2e0808627c23e994b695ab120dcb4
alexanderbie <bie.alexander@live.dk>
2023-10-06 11:20:18 +0200

    Back end update

--- back_end.py
+++ back_end.py
@@ -1,2 +1,30 @@
-# Her skal der flækkes noget back-end
+import csv
+import sys
+
+def main():
+    import_csv_database(sys.argv[1])
+
+def import_csv_database(database):
+    """Import the CSV database"""
+    # Define fieldnames
+    fieldnames = ["Film Titel", "Fede Skurke", "Bar Hud", "Vilde Våben", "One Liners", "Episk Action", "Total Score"]
+
+    # Open the CSV file
+    try:
+        with open(database, newline='') as csvfile:
+            reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+            for row in reader:
+                for key in row.keys():
+                    row['Film Titel'] = row['Film Titel'].removeprefix('[')
+                    row['Total Score'] = row['Total Score'].removesuffix(']')
+                print(row['Film Titel'], row['Total Score'])
+
+    # Look for
+    except FileNotFoundError:
+        sys.exit(f"Could not read {read_file}.")
+
+if __name__ =="__main__":
+    main()
+
+
 
