0fdd20800e86351dc37ee101e0e2bfb0e061423b
danielxthoegersen <daniel_loke@icloud.com>
2023-10-06 11:17:30 +0200

    more updates

--- front_end.py
+++ front_end.py
@@ -1,14 +1,16 @@
 import customtkinter
-import csv
 from tkinter import *
 from PIL import Image
 import os
 
+# Definer CTk tema
 customtkinter.set_appearance_mode("dark")
 
+
+# Lav vindue
 class App(customtkinter.CTk):
-    width = 875
-    height = 500
+    width = 1440
+    height = 900
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -17,34 +19,31 @@ class App(customtkinter.CTk):
         self.geometry(f"{self.width}x{self.height}")
         self.resizable(False, False)
 
+        # Angiv baggrundsbillede til vindue
         current_path = os.path.dirname(os.path.realpath(__file__))
-        self.bg_image = customtkinter.CTkImage(Image.open(current_path + "/media/hakke_bkg.png"),
+        self.bg_image = customtkinter.CTkImage(Image.open(current_path + "/media/highscore.png"),
                                                size=(self.width, self.height))
         self.bg_image_label = customtkinter.CTkLabel(self, image=self.bg_image, text="")
         self.bg_image_label.grid(row=0, column=0)
 
+
+
+        # Definer text_label
+        text_label = customtkinter.CTkLabel(self, 
+                                                   state="normal", 
+                                                   text="Film titel 1, 8, 4, 6, 4, 6, 56", 
+                                                   width=900, 
+                                                   height=400,
+                                                   text_color="black", 
+                                                   fg_color="white",
+                                                   font=("Helvetica", 30)
+                                                   )
     
+        # Indsæt text_label i vindue
+        text_label.place(relx=0.5, rely=0.7, anchor=CENTER)
 
-        button = customtkinter.CTkButton(self, text="HAK!", text_color="black", fg_color="white", hover_color="gray", bg_color="white", border_width=0, width=185)
 
-        score_entry1 = customtkinter.CTkEntry(self, placeholder_text="...", width=40, text_color="black", fg_color="white", bg_color="white", border_width=0, justify="center")
-        score_entry2 = customtkinter.CTkEntry(self, placeholder_text="...", width=40, text_color="black", fg_color="white", bg_color="white", border_width=0, justify="center")
-        score_entry3 = customtkinter.CTkEntry(self, placeholder_text="...", width=40, text_color="black", fg_color="white", bg_color="white", border_width=0, justify="center")
-        score_entry4 = customtkinter.CTkEntry(self, placeholder_text="...", width=40, text_color="black", fg_color="white", bg_color="white", border_width=0, justify="center")
-        score_entry5 = customtkinter.CTkEntry(self, placeholder_text="...", width=40, text_color="black", fg_color="white", bg_color="white", border_width=0, justify="center")
-
-        film_entry = customtkinter.CTkEntry(self, placeholder_text="...", text_color="black", fg_color="white", bg_color="white", border_width=0, justify="center")
-
-        total_score_label = customtkinter.CTkLabel(self, state="normal", text="???", width=40, text_color="gray", fg_color="white")
-
-        film_entry.place(relx=0.21, rely=0.7125, anchor=CENTER)
-        score_entry1.place(relx=0.387, rely=0.7125, anchor=CENTER)
-        score_entry2.place(relx=0.478, rely=0.7125, anchor=CENTER)
-        score_entry3.place(relx=0.57, rely=0.7125, anchor=CENTER)
-        score_entry4.place(relx=0.657, rely=0.7125, anchor=CENTER)
-        score_entry5.place(relx=0.743, rely=0.7125, anchor=CENTER)
-        total_score_label.place(relx=0.834, rely=0.712, anchor=CENTER)
-        button.place(relx=0.5, rely=0.865, anchor=CENTER)
+        # Indsæt sort_by fede_skurke
 
 
 
